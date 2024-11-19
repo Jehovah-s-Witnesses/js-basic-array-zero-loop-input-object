26f2c60754f3d941c4bde4c54aaa1b8003aad9da
gribVolnushka <burgersqueen98@gmail.com>
2024-11-19 23:22:19 +0200

    created new structure for project

--- src/index.js
+++ src/index.js
@@ -1,3 +1,48 @@
-export const loopCalculator = {};
+// Export const loopCalculator = {};
+//
+// Window.loopCalculator = loopCalculator;
+const loopCalculator = {
+  arr: [],
+  getInput() {
+    let userQuestion;
 
-window.loopCalculator = loopCalculator;
+    do {
+      userQuestion = prompt('Enter a number');
+
+      if (userQuestion === 'stop') {
+        break;
+      }
+
+      if (isNaN(userQuestion) || userQuestion === '') {
+        continue;
+      }
+
+      this.arr.push(+userQuestion);
+    } while (true)
+  },
+  getSum() {
+    let sumAdd = 0;
+    for (let key of this.arr) {
+      sumAdd += key;
+    }
+    return sumAdd;
+  },
+  getMultiplication() {
+    let sumMultiplication = 1;
+    for (let key of this.arr) {
+      sumMultiplication *= key;
+    }
+    return sumMultiplication;
+  },
+  clearData() {
+    this.arr.length = 0;
+  },
+  getMin() {
+    return Math.min(...this.arr);
+  },
+  getMax() {
+    return Math.max(...this.arr);
+  },
+};
+loopCalculator.getInput();
+alert(loopCalculator.arr);
